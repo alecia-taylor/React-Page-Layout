4e983df5bde5936071c6578d0cb956877e851c43
Alecia Taylor <alecia.taylor0@gmail.com>
2025-04-29 14:14:55 -0400

    add EmployeeList and EmployeeListItem components

---
+++ ReactLayout/ReactLayout/src/components/EmployeeListItem.jsx
@@ -0,0 +1,9 @@
+export default function EmployeeListItem() {
+    return (
+      <div className='employeeListItem'>
+      <div>EmployeeListItem</div>
+      <h5>Employee Name</h5>
+      <h6>Employee Title</h6>
+      </div>
+    )
+  }
