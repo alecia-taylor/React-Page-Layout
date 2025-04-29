2db605a81a66c89191fbda88c1bb081b41271880
Alecia Taylor <alecia.taylor0@gmail.com>
2025-04-29 14:21:47 -0400

    add Header component with basic structure

--- ReactLayout/ReactLayout/src/components/EmployeePage.jsx
+++ ReactLayout/ReactLayout/src/components/EmployeePage.jsx
@@ -1,1 +1,22 @@
+import React from 'react'
+import Header from './Header'
 
+export default function EmployeePage() {
+  return (
+    <div className="employeePage">
+    <div>EmployeePage</div>
+    <Header />
+    <div>
+      <h3>Employee Name</h3>
+      <p>Call Office</p>
+      <h6>781-000-0002</h6>
+      <p>Call Mobile</p>
+      <h6>781-000-0002</h6>
+      <p>SMS</p>
+      <h6>781-000-0002</h6>
+      <p>Email</p>
+      <h6>tishana.trainor@perscholas.org</h6>
+    </div>
+    </div>
+  )
+}
